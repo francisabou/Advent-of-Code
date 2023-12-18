213b5840b6dfead6c4de60c3d2532e67a7fa75d1
francisabou <152104510+francisabou@users.noreply.github.com>
2023-12-18 03:42:52 +0200

    update

---
+++ Day 1/part2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+
+
+
+int main(){
+
+//std::ifstream input("part1_input.txt");
+std::ifstream input("part2_example.txt");
+std::string line;
+int cascii;
+int sum=0;
+
+while(std::getline(input,line)){
+    for(int j=0;j<line.length();j++){
+        if(line.at(j)<58&&line.at(j)>47){
+            sum+=10*(line.at(j)-48);
+            break;
+        }
+    }
+
+    for(int j=line.length()-1;j>-1;j--){
+        if(line.at(j)<58&&line.at(j)>47){
+            sum+=line.at(j)-48;
+            break;
+        }
+    }
+}
+
+std::cout<<sum<<"\n";
+
+    return 0;
+}
